141a4a191a95d0c059b9dc50cb517c25ffddaaf5
gositonyourbed <115512014+gositonyourbed@users.noreply.github.com>
2022-10-10 17:49:24 -0700

    Add files via upload

---
+++ Random Number Generator/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = Int((100 * Rnd()) + 1)
+    End Sub
+End Class
